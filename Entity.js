13edc48fed80780d477eca3af5dc8eefef781653
unknown <SoftwareSolutions1@.(none)>
2011-06-27 11:04:17 -0500

    Edited Entity.js

--- Entity.js
+++ Entity.js
@@ -9,9 +9,9 @@
     //TODO: Implement
   }
   Entity.VERSION = '1.0';
-  
+
   Entity.Base = {};
-  
+
   Entity.Model = {
     factory: function() {
 	  function object() {
@@ -19,24 +19,22 @@
       }
 	  return object;
 	},
-	
+
     create: function() {
 	  var object = Entity.Model.factory();
-	  if(!Entity.Type.isSet(object.init)) {
-		  object.prototype.init = function(){};
-	  }
+
 	  object.prototype = Entity.Base;
 	  object.prototype.constructor = object;
-	  
+
       Entity.Model.extend(object, arguments[0]);
-	  
+
 	  if(!Entity.Type.isSet(object.prototype.init)) {
 		  object.prototype.init = function(){};
 	  }
 
 	  return object;
     },
-	
+
     extend: function(object, args) {
       for (var method in args) {
         object.prototype[method] = args[method];
@@ -44,19 +42,19 @@
       return object.prototype;
     },
   };
-  
+
   Entity.Enumerable = {
     toArray: function(args) {
 	  var length = args.length;
 	  var result = new Array(length);
-	
+
 	  for(var i = 0; i < length; i++) {
 	    result[i] = args[i];
 	  }
 	  return result;
 	}
   };
-  
+
   Entity.Type = {
     isSet: function(object) {
        return typeof object !== "undefined" && object !== null;
